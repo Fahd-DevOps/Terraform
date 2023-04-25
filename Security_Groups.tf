608ac8511fba1ec577467c5b91ba0326b7942380
Fahd Khaled <64971995+Fahd-DevOps@users.noreply.github.com>
2023-04-25 16:16:24 +0200

    Add Security Groups

---
+++ Security_Groups.tf
@@ -0,0 +1,102 @@
+# Create Security Group for SSH Access from Anywhere open to all
+resource "aws_security_group" "public_ssh" {
+  name        = "public_ssh"
+  description = "Allow SSH Access from Anywhere"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = var.TAG
+  }
+}
+
+# Create Security Group for SSH and Port 3000 from VPC CIDR
+resource "aws_security_group" "private_ssh_3000" {
+  name        = "private_ssh_3000"
+  description = "Allow SSH and Port 3000 from VPC CIDR"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    description = "ssh and custom from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [module.network.vpc_cidr]
+  }
+
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [module.network.vpc_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = var.TAG
+  }
+}
+
+# Create a security group for RDS
+resource "aws_security_group" "rds_security_group" {
+  name_prefix = "rds_sg_"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [module.network.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a security group for Elasticache
+resource "aws_security_group" "elasticache_security_group" {
+  name_prefix = "elasticache_sg_"
+  vpc_id      = module.network.vpc_id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [module.network.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
