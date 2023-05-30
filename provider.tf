d0692382075d9e377f21041b0aa6b113ff87ef6a
Fahd-DevOps <fahdkhaled777@gmail.com>
2023-05-30 09:41:33 -0700

    add k8s

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = var.REGION
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.private_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.private_cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.private_cluster.token
+  # load_config_file       = false
+}
