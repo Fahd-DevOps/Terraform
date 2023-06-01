a154e29823c4d8a8badcee4c7a6ade70a5d4c1fe
Fahd Khaled <64971995+Fahd-DevOps@users.noreply.github.com>
2023-06-01 04:29:33 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,4 +7,5 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.private_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.private_cluster.token
   # load_config_file       = false
+  # timeout = "10m"
 }
